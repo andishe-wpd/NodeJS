546f2672872eb7939db389bc51edc465066558b7
andishe <a.abbasian@pixaldo.com>
2023-01-19 11:50:52 +0330

    added HTML response

--- 3-nodeBasics/3-NodeResponse/app.js
+++ 3-nodeBasics/3-NodeResponse/app.js
@@ -1,25 +1,13 @@
-// http, https, os, path, fs
-// const http = require('./' or '/' or global module)
 const http = require("http");
-
-// http.createServer()  // this needs a request listener so make it
-
-//  request in incomming message ans response is response
-// can used as  a inline ananymose funvtion
-// function requestListener(request , response){
-// }
-
-// alternativly i can use event driven articheture like this
-
 const server = http.createServer((request, response) => {
-  console.log(request.headers);
-  setTimeout(() => {
-    // console.log("exited");
-    // exited the process listening
-    // hard exit
-    // process.exit();
-  }, 3000);
-}); // this needs a request listener so make it
+  response.setHeader("conten-type", "text/html");
+  response.write("<html>");
+  response.write("<head><title>html response</title></head>");
+  response.write(
+    "<body><p>html response content</p><hr/><p>you can write whatever html content do you want</p></body>"
+  );
+  response.write("</html>");
+  response.end()
+});
 
-// turn on server listening
 server.listen(3000);
