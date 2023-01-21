7f32c1497fa3323f3b6a6d94de47575379224742
andishe <a.abbasian@pixaldo.com>
2023-01-21 20:56:02 +0330

    updated redirection and fs file maker

--- 3-nodeBasics/5-Redirection/app.js
+++ 3-nodeBasics/5-Redirection/app.js
@@ -1,9 +1,12 @@
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer((request, response) => {
   const url = request.url;
-
+  const METHOD = request.method;
+  console.log(METHOD);
+  // console.log(url === "/welcome" && METHOD === "POST");
   if (url === "/") {
-    response.setHeader("content-type", "text/html");
+    // response.setHeader("content-type", "text/html");
     response.write("<html>");
     response.write("<head><title>html response</title></head>");
     response.write(
@@ -11,17 +14,25 @@ const server = http.createServer((request, response) => {
     );
     response.write("</html>");
   }
-  if (url === "/welcome") {
+  if (url === "/welcome" && METHOD === "GET") {
+    console.log("fired");
     response.write("<html>");
     response.write("<head><title>not found </title></head>");
     response.write("<body><p>welcome to the welcome page </p></body>");
     response.write("</html>");
+    return response.end();
+  }
+  if (url === "/welcome" && METHOD === "POST") {
+    fs.writeFileSync("message.txt", "DoomsDay");
+    response.statusCode = 302;
+    response.setHeader("location", "/welcome");
+    return response.end();
   } else {
     response.write("<html>");
     response.write("<head><title>not found </title></head>");
     response.write("<body><p>cant find the page you lookin for </p></body>");
     response.write("</html>");
-    response.end();
+    return response.end();
   }
 });
 
