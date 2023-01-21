aaafe7ea38a22cf95e9875ff5575cc1f415a2ee0
andishe <a.abbasian@pixaldo.com>
2023-01-21 19:37:27 +0330

    added routing conditional

--- 3-nodeBasics/4-RoutingRequests/app.js
+++ 3-nodeBasics/4-RoutingRequests/app.js
@@ -1,13 +1,26 @@
 const http = require("http");
 const server = http.createServer((request, response) => {
-  response.setHeader("conten-type", "text/html");
-  response.write("<html>");
-  response.write("<head><title>html response</title></head>");
-  response.write(
-    "<body><p>html response content</p><hr/><p>you can write whatever html content do you want</p></body>"
-  );
-  response.write("</html>");
-  response.end();
+  const url = request.url;
+
+  if (url === "/") {
+    response.setHeader("conten-type", "text/html");
+    response.write("<html>");
+    response.write("<head><title>html response</title></head>");
+    response.write(
+      "<body><p>html response content</p><hr/><p>you can write whatever html content do you want</p></body>"
+    );
+    response.write("</html>");
+    response.end();
+  } else {
+    response.setHeader("conten-type", "text/html");
+    response.write("<html>");
+    response.write("<head><title>not found </title></head>");
+    response.write(
+      "<body><hr/><p>cant find the page you lookin for </p></body>"
+    );
+    response.write("</html>");
+    response.end();
+  }
 });
 
 server.listen(3000);
