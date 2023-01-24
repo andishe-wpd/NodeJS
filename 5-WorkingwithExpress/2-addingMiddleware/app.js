5bc3316f1d0c6fd7d3618a343a0959ea1432aa53
andishe <a.abbasian@pixaldo.com>
2023-01-24 23:43:11 +0330

    adding response

--- 5-WorkingwithExpress/2-addingMiddleware/app.js
+++ 5-WorkingwithExpress/2-addingMiddleware/app.js
@@ -4,14 +4,21 @@ const express = require("express");
 const app = express();
 
 app.use((request, response, next) => {
-    console.log(request.url)
-    response.end()
+    console.log('im in the middleware')
     next()
 });
 app.use((request, response, next) => {
-    console.log('request.url')
+    console.log('i wanna send a response in next middleware')
+    next()
+});
+app.use((request, response, next) => {
+    response.send('<h1>hello from express</h1>')
+
 });
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3000);
+// server.listen(3000);
+
+
+app.listen(3000)
