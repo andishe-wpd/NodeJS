5dd81738c30972ed5f4ac997fcae68a1cc28537d
andishe <a.abbasian@pixaldo.com>
2023-01-27 18:09:21 +0330

    added body parser on app.js

--- 5-WorkingwithExpress/4-courseReviewSofar/app.js
+++ 5-WorkingwithExpress/4-courseReviewSofar/app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
-const index = require('./routes/index')
-const user = require('./routes/user')
-const admin = require('./routes/admin')
-const notFound = require('./routes/404')
+const index = require("./routes/index");
+const user = require("./routes/user");
+const admin = require("./routes/admin");
+const notFound = require("./routes/404");
+const bodyParser = require("body-parser");
 
-
-
-app.use(index)
-app.use(user)
-app.use('/admin',admin)
-app.use(notFound)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(index);
+app.use(user);
+app.use("/admin", admin);
+app.use(notFound);
 
 app.listen(3000);
