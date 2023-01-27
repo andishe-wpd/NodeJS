c8ec4b5340069a7a962ff1000a16a30c9033d691
andishe <a.abbasian@pixaldo.com>
2023-01-27 19:16:32 +0330

    serve .html

--- 5-WorkingwithExpress/5-HTMLServe/routes/404.js
+++ 5-WorkingwithExpress/5-HTMLServe/routes/404.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const path = require("path");
 
-router.use('/', (request, response, next)=>{
-    response.status(404).send('<h1>404 page , not found</h1>')
-})
+router.use("/", (request, response, next) => {
+  // response.status(404).send('<h1>404 page , not found</h1>')
+  response
+    .status(404)
+    .sendFile(path.join(__dirname, "../", "views", "404.html"));
+});
 
-module.exports = router
+module.exports = router;
--- 5-WorkingwithExpress/5-HTMLServe/routes/shop.js
+++ 5-WorkingwithExpress/5-HTMLServe/routes/shop.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
+const path = require("path");
 const router = express.Router();
 
 router.get("/", (request, response, next) => {
   // console.log("im in the middleware");
-  response.send("<h1>this is the resopnse</h1>");
+  // response.send("<h1>this is the resopnse</h1>");
+  response.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
