66a09d8607922074b28db87e855879e6b09f8d1e
andishe <a.abbasian@pixaldo.com>
2023-01-27 18:09:44 +0330

    added form data getter and setter on admin

--- 5-WorkingwithExpress/4-courseReviewSofar/routes/admin.js
+++ 5-WorkingwithExpress/4-courseReviewSofar/routes/admin.js
@@ -1,18 +1,19 @@
-const { response } = require("express")
-const express = require("express")
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.send('<h1>admin page</h1>')
-})
-router.get('/add-product',(req,res)=>{
-    res.send('<form action="add-product-result" method="POST"><input type:"text" name:"title" /><button type:"submit" >add product title</button></form>')
-})
-router.post('/add-product-result',(req,response)=>{
-    response.redirect('/')
-    // res.send('<h1>adminresult page</h1>')
-})
+router.get("/", (req, res) => {
+  res.send("<h1>admin page</h1>");
+});
+router.get("/add-product", (req, res) => {
+  res.send(
+    '<form action="add-product-result" method="POST"><input type="text" name="title" /><button type="submit" >add product title</button></form>'
+  );
+});
+router.post("/add-product-result", (request, response) => {
+  console.log(request.body);
+  response.redirect("/");
+  // res.send('<h1>adminresult page</h1>')
+});
 
-
-module.exports = router
+module.exports = router;
