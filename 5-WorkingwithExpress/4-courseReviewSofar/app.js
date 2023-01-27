9c8aa532d13c4c5a2f1d2c0c21c55de55a05846a
andishe <a.abbasian@pixaldo.com>
2023-01-27 13:15:24 +0330

    added index route

--- 5-WorkingwithExpress/4-courseReviewSofar/app.js
+++ 5-WorkingwithExpress/4-courseReviewSofar/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const index = require('./routes/index')
 const user = require('./routes/user')
 const admin = require('./routes/admin')
 const notFound = require('./routes/404')
 
 
 
+app.use(index)
 app.use(user)
 app.use(admin)
 app.use(notFound)
