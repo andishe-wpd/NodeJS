9823f5217f1bb4cf1f1d2bbf53775a57e9cd795f
andishe <a.abbasian@pixaldo.com>
2023-02-03 21:11:43 +0330

    added static directory handler

--- 5-WorkingwithExpress/Z-wrapUp/app.js
+++ 5-WorkingwithExpress/Z-wrapUp/app.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const homePage = require("./routes/homePage");
-const order = require('./routes/order')
+const order = require("./routes/order");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(homePage);
 app.use(order);
