59d5f2edd0cb3ebf0664ba476e9c4bb7c488798b
andishe <a.abbasian@pixaldo.com>
2023-02-03 22:55:56 +0330

    formatted

--- 5-WorkingwithExpress/Z-wrapUp/routes/404.js
+++ 5-WorkingwithExpress/Z-wrapUp/routes/404.js
@@ -1,13 +1,11 @@
-const fs = require('fs')
+const fs = require("fs");
 const express = require("express");
 const Router = express.Router();
-const Root = require('../helper/rootPath')
-const path  = require('path')
+const Root = require("../helper/rootPath");
+const path = require("path");
 
 Router.use("/", (request, response) => {
-    response.status(404).sendFile(path.join(Root, 'views', '404.html'))
+  response.status(404).sendFile(path.join(Root, "views", "404.html"));
 });
 
-
-
-module.exports = Router
+module.exports = Router;
